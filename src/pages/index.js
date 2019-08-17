897d196ff9e477c83ded5c36b1ca0ce1fff5315f
Filip <f.cieminski@gmail.com>
2019-08-17 12:17:33 +0200

    better typewrite handle

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,35 +5,38 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Header from "../components/Header"
 import "../styles/styles.scss"
+import "../styles/home.scss"
 
 const IndexPage = () => {
-  const [letters, setLetters] = useState("")
+  const [letters, setLetters] = useState(
+    "There are only 10 types of people in the world"
+  )
 
   useEffect(() => {
     typeText()
   }, [])
 
+  var iArrLength = letters.length
+  let startPos = 0
+
   const typeText = () => {
-    let count = 0
-    let msg = "Witaj nieznajomy"
-    let finalMsg = msg.split("")
-    let typeWrite = setInterval(() => {
-      setLetters(prevLetters => prevLetters.concat(finalMsg[count]))
-      count++
-      if (count === msg.length) {
-        clearInterval(typeWrite)
-      }
-    }, 100)
+    let newText = letters
+    setLetters(letters.substring(0, startPos) + "|")
+    if (startPos++ === newText.length) {
+      startPos = 0
+    } else {
+      setTimeout(typeText, 100)
+    }
   }
 
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <SEO title="Home" />
       <div className="hero-image">
         <h1 className="hero-image__text">{letters}</h1>
       </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      {/* <Link to="/page-2/">Go to page 2</Link> */}
     </div>
   )
 }
