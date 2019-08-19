e30be6f2b463cc3bd46db997815712f09cc168ca
Filip <f.cieminski@gmail.com>
2019-08-19 22:23:31 +0200

    styling mockup data

--- src/pages/blog-page.js
+++ src/pages/blog-page.js
@@ -1,19 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import SEO from "../components/seo"
 import Header from "../components/Header"
+import "../styles/blogpage.scss"
 
-const BlogPage = () => (
-  <>
-    <SEO title="Page two" />
-    <Header />
-    <main className="main">
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
-    </main>
-  </>
-)
+import { data } from "../components/mockup"
+
+const BlogPage = () => {
+  const [posts, setData] = useState(data)
+  return (
+    <>
+      <SEO title="Page two" />
+      <Header />
+      <main className="main">
+        <section>
+          <div>
+            {posts.map(post => (
+              <article className="post">
+                <div className="post__image">
+                  <img src={post.image} />
+                </div>
+                <div className="post__content">
+                  <div className="post__content-info-box">
+                    <span className="material-icons material-icons--posts">
+                      expand_more
+                    </span>
+                    <p>{post.title}</p>
+                  </div>
+                  <div className="post__content-description">
+                    {post.description.substring(0, 100)} ...read more
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
 
 export default BlogPage
