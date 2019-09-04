3725ca1193571c3f97179ada63be5d9a70e72cca
Filip <f.cieminski@gmail.com>
2019-09-04 22:20:05 +0200

    polish date (i know i don't have to do that like this ;)  )

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,6 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 
 const Posts = props => {
+  let dateFormatter = new Intl.DateTimeFormat("pl", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  })
+
+  let newDate = []
+  let switchDate = props.frontmatter.date.split(",")[0].split(".")
+  newDate.push(switchDate[1], switchDate[0], switchDate[2])
+
   return (
     <article className="post">
       <div
@@ -12,22 +22,20 @@ const Posts = props => {
       ></div>
       <div className="post__content">
         <div className="post__content-info-box">
-          <span className="material-icons material-icons--posts">
-            expand_more
-          </span>
           <h3>{props.frontmatter.title}</h3>
         </div>
         <div className="post__content-description">
-          {props.excerpt.substring(0, 100)}
-          <Link state={{ ...props }} to={`blog${props.frontmatter.path}`}>
-            ...read more
-          </Link>
+          {props.excerpt.substring(0, 100)}...
         </div>
+        <Link state={{ ...props }} to={`blog${props.frontmatter.path}`}>
+          <i className="material-icons material-icons--posts">expand_more</i>
+          {new Date().to}
+        </Link>
         <div className="post--footer">
           <i className="material-icons">watch_later</i>
-          <div className="">{`${props.timeToRead}m | ${
-            props.frontmatter.date.split(",")[0]
-          }`}</div>
+          <div className="">{`${props.timeToRead}m | ${dateFormatter.format(
+            new Date(newDate.join(" "))
+          )}`}</div>
         </div>
       </div>
     </article>
