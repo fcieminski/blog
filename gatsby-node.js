c8a4d27541540106b878cd92b4f5d8df03e20ce6
Filip <f.cieminski@gmail.com>
2019-09-04 23:12:38 +0200

    promise all to handle tags page generation, and simple tags page to list tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,8 +4,9 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   const blogPost = path.resolve(`./src/pages/blog.js`)
+  const tagSite = path.resolve(`./src/pages/tags.js`)
 
-  return graphql(`
+  const postFetch = graphql(`
     {
       allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
@@ -23,9 +24,9 @@ exports.createPages = ({ graphql, actions }) => {
       throw result.errors
     }
     const posts = result.data.allMarkdownRemark.edges
+
     posts.forEach(({ node }) => {
       const path = `/blog${node.frontmatter.path}`
-
       createPage({
         path,
         component: blogPost,
@@ -36,12 +37,42 @@ exports.createPages = ({ graphql, actions }) => {
       })
     })
   })
+
+  const tagsFetch = graphql(`
+    {
+      tags: allMarkdownRemark(limit: 2000) {
+        group(field: frontmatter___tags) {
+          fieldValue
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+    const tags = result.data.tags.group
+
+    tags.forEach(tag => {
+      const path = `/tags/${tag.fieldValue
+        .toLowerCase()
+        .split(".")
+        .join("-")}`
+      createPage({
+        path,
+        component: tagSite,
+        context: {
+          tag: tag.fieldValue,
+        },
+      })
+    })
+  })
+  return Promise.all([postFetch, tagsFetch])
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
-  if (node.internal.type === `mdx`) {
+  if (node.internal.type === `md`) {
     const value = createFilePath({ node, getNode })
     createNodeField({
       name: `path`,
